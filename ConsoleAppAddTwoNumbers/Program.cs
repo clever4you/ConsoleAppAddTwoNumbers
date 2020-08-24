e9f89beef827db5346574a8d15b94766eb284840
evgeny.kamiak <evgeny.kamiak@hiqo-solutions.com>
2020-08-24 10:55:27 +0300

    Add project files.

---
+++ ConsoleAppAddTwoNumbers/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ConsoleAppAddTwoNumbers
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result = null;
+            bool isNeedToAddOne = false;
+            while (l1 != null || l2 != null)
+            {
+                var i = (l1?.val ?? 0) + (l2?.val ?? 0);
+                if (isNeedToAddOne)
+                {
+                    i++;
+                    isNeedToAddOne = false;
+                }
+                if (i > 9)
+                {
+                    isNeedToAddOne = true;
+                    i -= 10;
+                }
+                result = new ListNode(i, result);
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+
+            if (isNeedToAddOne)
+            {
+                result = new ListNode(1, result);
+            }
+
+            return ReverseList(result);
+        }
+
+        private ListNode ReverseList(ListNode root)
+        {
+            ListNode p = root;
+            ListNode n = null;
+            while (p != null)
+            {
+                ListNode tmp = p.next;
+                p.next = n;
+                n = p;
+                p = tmp;
+            }
+            return n;
+        }
+    }
+}
---
+++ ConsoleAppAddTwoNumbersTests/SolutionTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Xml;
+using NUnit.Framework;
+using ConsoleAppAddTwoNumbers;
+using NuGet.Frameworks;
+
+namespace ConsoleAppAddTwoNumbersTests
+{
+    [TestFixture()]
+    public class SolutionTests
+    {
+        private Solution _solution;
+
+        [SetUp()]
+        public void Setup()
+        {
+            _solution = new Solution();
+        }
+
+        private ListNode CreateNodeFromString(string s)
+        {
+            ListNode ln = null;
+            foreach (char c in s)
+            {
+                int i = int.Parse(c.ToString());
+                ln = ln != null
+                    ? new ListNode(i, ln)
+                    : new ListNode(i);
+            }
+            return ln;
+        }
+
+        [Test]
+        [TestCase("123", "234", "357")]
+        [TestCase("5", "5", "10")]
+        [TestCase("342", "465", "807")]
+        [TestCase("81", "0", "81")]
+        public void AddTwoNumbersTest(string node1, string node2, string expected)
+        {
+            var listNode1 = CreateNodeFromString(node1);
+            var listNode2 = CreateNodeFromString(node2);
+            var result = _solution.AddTwoNumbers(listNode1, listNode2);
+            var actual = ListNodeToString(result);
+            Assert.AreEqual(expected, actual);
+        }
+
+        private string ListNodeToString(ListNode listNode)
+        {
+            string result = listNode.val.ToString();
+            while (listNode.next != null)
+            {
+                listNode = listNode.next;
+                result = listNode.val + result;
+            }
+            return result;
+        }
+    }
+}
